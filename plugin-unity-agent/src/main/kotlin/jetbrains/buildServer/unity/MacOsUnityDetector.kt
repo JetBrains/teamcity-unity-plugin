a6597d1ad52cb86303728d7926d7d703fb917cd6
Dmitry Tretyakov <dtretyakov@gmail.com>
2018-12-18 16:24:21 +0100

    Fix unity detector on macOS

--- plugin-unity-agent/src/main/kotlin/jetbrains/buildServer/unity/MacOsUnityDetector.kt
+++ plugin-unity-agent/src/main/kotlin/jetbrains/buildServer/unity/MacOsUnityDetector.kt
@@ -9,7 +9,7 @@ import java.lang.Exception
 class MacOsUnityDetector : UnityDetector {
 
     override fun findInstallations() = sequence {
-        getHintPaths().forEach {  path ->
+        getHintPaths().forEach { path ->
             val executable = getEditorPath(path)
             if (!executable.exists()) return@forEach
 
@@ -17,8 +17,17 @@ class MacOsUnityDetector : UnityDetector {
             if (!plistFile.exists()) return@forEach
             val config = XMLPropertyListConfiguration(plistFile)
 
+            // Unity version looks like that: 2017.1.1f1
+            // where suffix could be the following:
+            // * a  - alpha
+            // * b  - beta
+            // * p  - patch
+            // * rc - release candidate
+            // * f  - final
             val version = config.getString("CFBundleVersion")
-            if (version.isNullOrEmpty()) return@forEach
+                    ?.split("[abcfpr]")
+                    ?.firstOrNull()
+                    ?: return@forEach
             try {
                 yield(Version.valueOf(version) to path)
             } catch (e: Exception) {
@@ -34,8 +43,8 @@ class MacOsUnityDetector : UnityDetector {
 
         // The convention to install multiple Unity versions is
         // to use suffixes for Unity directory, e.g. Unity_4.0b7
-        directory.listFiles {
-            file -> file.isDirectory && file.name.startsWith("Unity")
+        directory.listFiles { file ->
+            file.isDirectory && file.name.startsWith("Unity")
         }?.let { files ->
             yieldAll(files.asSequence())
         }
@@ -43,8 +52,8 @@ class MacOsUnityDetector : UnityDetector {
         // Unity Hub installs editors under Unity/Hub/Editor directory,
         // e.g. Unity/Hub/Editor/2018.1.9f2
         val unityHub = File(directory, "Unity/Hub/Editor")
-        unityHub.listFiles {
-            file -> file.isDirectory
+        unityHub.listFiles { file ->
+            file.isDirectory
         }?.let { files ->
             yieldAll(files.asSequence())
         }
---
+++ plugin-unity-agent/src/test/kotlin/jetbrains/buildServer/unity/UnityVersionsDetectorTest.kt
@@ -0,0 +1,22 @@
+package jetbrains.buildServer.unity
+
+import com.intellij.openapi.util.SystemInfo
+import org.testng.SkipException
+import org.testng.annotations.Test
+
+class UnityVersionsDetectorTest {
+
+    @Test
+    fun testFindInstallations() {
+        val detector = when {
+            SystemInfo.isWindows -> WindowsUnityDetector()
+            SystemInfo.isMac -> MacOsUnityDetector()
+            else -> null
+        } ?: throw SkipException("Not supported platform")
+
+        val installations = detector.findInstallations()
+        println(installations.joinToString("\n") { (version, path) ->
+            "$version in $path"
+        })
+    }
+}
